c53f347b50ce72b78898fa9d1a9ec5823c84e55d
mlherd <h.meliherdogan@gmail.com>
2016-11-15 11:41:10 -0800

    Update motor_controller.py

--- motor_controller.py
+++ motor_controller.py
@@ -1,34 +1,94 @@
+import sys
 import time
 import RPi.GPIO as GPIO
 
-import motor_controller as MC
-import ultrasonic as US
+class MotorController:
 
-GPIO.setmode(GPIO.BCM)
-vikingbotMotors = MC.MotorController()
-ultrasonicSensorBack = US.Ultrasonic()
+        def __init__(self):
+                self.PinForwardW1 = 13
+                self.PinBackwardW1 = 26
+                self.PinForwardW2 =  6
+                self.PinBackwardW2 = 5
+                self.PinPWM_W1 = 22
+                self.PinPWM_W2 = 27
+                self.sleeptime = 1
+                self.p1 = None
+                self.p2 = None
 
-#GPIO.cleanup()
+        def set_Pins(self, pin1, pin2, pin3, pin4, pin5, pin6):
+                self.PinForwardW1 = pin1
+                self.PinBackwardW1 = pin2
+                self.PinForwardW2 =  pin3
+                self.PinBackwardW2 = pin4
+                self.PinPWM_W1 = pin5
+                self.PinPWM_W2 = pin6
 
-vikingbotMotors.setup_GPIO(1,0)
-vikingbotMotors.setup_PWM()
-vikingbotMotors.start_PWM()
-vikingbotMotors.set_motorSpeed(90,90)
-vikingbotMotors.set_SleepTime(0.030)
-#vikingbotMotors.goForward()
-#vikingbotMotors.sset_SleepTime(1)
-#vikingbotMotors.turnLeft()
-#vikingbotMotors..set_SleepTime(1)
-#vikingbotMotors.turnRight()
-#vikingbotMotors.set_SleepTime(2)
-#vikingbotMotors.goBack()
+        def set_SleepTime(self, time):
+                self.sleeptime = time
 
-ultrasonicSensorBack.setup_GPIO()
+        def setup_GPIO(self, mode, warning):
+                if mode == 1:
+                        GPIO.setmode(GPIO.BCM)
+                else:
+                        GPIO.setmode(GPIO.BOARD)
 
-while(True):
+                if warning == 1:
+                        GPIO.setwarnings(True)
+                else:
+                        GPIO.setwarnings(False)
 
-        if (ultrasonicSensorBack.get_distance() > 10):
-                vikingbotMotors.goBack()
+                GPIO.setup(self.PinForwardW1, GPIO.OUT)
+                GPIO.setup(self.PinBackwardW1, GPIO.OUT)
+                GPIO.setup(self.PinForwardW2, GPIO.OUT)
+                GPIO.setup(self.PinBackwardW2, GPIO.OUT)
+                GPIO.setup(self.PinPWM_W1, GPIO.OUT)
+                GPIO.setup(self.PinPWM_W2, GPIO.OUT)
 
-GPIO.cleanup()
+        def setup_PWM(self):
+                self.p1 = GPIO.PWM(self.PinPWM_W1, 100)
+                self.p2 = GPIO.PWM(self.PinPWM_W2, 100)
+
+        def start_PWM(self):
+                self.p1.start(0)
+                self.p2.start(0)
+
+        def set_motorSpeed(self, speedW1, speedW2):
+                self.p1.ChangeDutyCycle(speedW1)
+                self.p2.ChangeDutyCycle(speedW2)
+                
+        def turnLeft(self):
+                GPIO.output(self.PinForwardW1, GPIO.LOW)
+                GPIO.output(self.PinBackwardW1, GPIO.HIGH)
+                GPIO.output(self.PinForwardW2, GPIO.HIGH)
+                GPIO.output(self.PinBackwardW2, GPIO.LOW)
+                print "turning left"
+                time.sleep(self.sleeptime)
+                GPIO.output(self.PinBackwardW1, GPIO.LOW)
+                GPIO.output(self.PinForwardW2, GPIO.LOW)
+
+        def goForward(self):
+                GPIO.output(self.PinForwardW1, GPIO.HIGH)
+                GPIO.output(self.PinForwardW2, GPIO.HIGH)
+                GPIO.output(self.PinBackwardW1, GPIO.LOW)
+                GPIO.output(self.PinBackwardW2, GPIO.LOW)
+                print "going forward"
+                time.sleep(self.sleeptime)
+                GPIO.output(self.PinForwardW1, GPIO.LOW)
+                GPIO.output(self.PinForwardW2, GPIO.LOW)
+
+        def goBack(self):
+                GPIO.output(self.PinBackwardW1, GPIO.HIGH)
+                GPIO.output(self.PinBackwardW2, GPIO.HIGH)
+                GPIO.output(self.PinForwardW1, GPIO.LOW)
+                GPIO.output(self.PinForwardW2, GPIO.LOW)
+                print "going back"
+                time.sleep(self.sleeptime)
+                GPIO.output(self.PinBackwardW1, GPIO.LOW)
+                GPIO.output(self.PinBackwardW2, GPIO.LOW)
+
+        def cleanup(self):
+                GPIO.cleanup()
+
+
+
 
